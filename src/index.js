c4cf05f84bcd53b66971cf6833693cc0aa87e730
Chris Rebuck <rebuckc2308@gmail.com>
2021-12-15 00:16:37 -0600

    Completed functionality of adding and storing tracks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
 app.use(express.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri
 
